b08c2e79ef084607e36c67271572cbf3d653e911
WitoldSierka <witsierka@gmail.com>
2022-10-11 19:28:29 +0200

    restore https service

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import HttpsService from './components/HttpsService';
 
 import {
   createBrowserRouter,
@@ -12,6 +13,11 @@ import {
 
 function App() {
 
+  async function myFunction() {
+    const card = await HttpsService.getCardById(386616);
+    console.log(card);
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -20,6 +26,7 @@ function App() {
           <Link to={"/about"} >O nas </Link>
           <Link to={"/card/123456"} >Karta 123456 </Link>
         </div>
+          <button onClick={myFunction}>click me</button>
           <h2>Tu będzie menu</h2>
           <Routes>
             <Route path='/' element={<div>Hello world!</div>} />
